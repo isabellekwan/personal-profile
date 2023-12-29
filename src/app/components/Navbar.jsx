1da3758e7cf6f61dfdf4e9c51347b0b2d21e876a
Isabelle Kwan <bellekwan9@gmail.com>
2023-12-28 21:31:15 -0800

    editing mobile navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import Link from 'next/link'
 import NavLink from './NavLink'
 
@@ -18,12 +19,20 @@ const navLinks = [
 ]
 
 const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#e9e5e1]">
         <div className="flex flex-wrap items-center justify-between mx-auto p-8">
             <Link href={'/'} className="text-2xl md:text-5xl text-[#5d4d3d] font-semibold">
                 Logo
             </Link>
+            <div className="mobile-menu block md:hidden">
+                {
+                    navbarOpen ? {
+                        
+                    }
+                }
+            </div>
             <div className="menu hidden md:block md:w-auto" id="navbar">
                 <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                     {
